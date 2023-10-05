936ecca53e6f443859c888a641e15118029c3882
neuefische <info@neuefische.de>
2023-10-05 15:42:19 +0200

    add backend hello world method

---
+++ src/main/java/de/neuefische/backend/MyController.java
@@ -0,0 +1,16 @@
+package de.neuefische.backend;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+@RequiredArgsConstructor
+public class MyController {
+
+    @GetMapping
+    String getHelloString() {
+        return "Hello World";
+    }
+}
